1ac4768502d6da11bfd9d8f14c12d49a488fb663
ridurahman <lindaridu@gmail.com>
2024-01-29 22:26:10 +0600

    Uploaded

---
+++ heightOfBinaryTree.js
@@ -0,0 +1,52 @@
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function buildBinaryTree(values) {
+  if (values.length === 0) {
+    return null;
+  }
+  const root = new TreeNode(values[0]);
+  const queue = [root];
+  let i = 1;
+
+  while (i < values.length) {
+    const currentNode = queue.shift();
+    if (values[i] !== null) {
+      currentNode.left = new TreeNode(values[i]);
+      queue.push(currentNode.left);
+    }
+    i++;
+
+    if (values[i] !== null) {
+      currentNode.right = new TreeNode(values[i]);
+      queue.push(currentNode.right);
+    }
+    i++;
+  }
+
+  return root;
+}
+
+function maxHeight(node) {
+  if (node == null) return 0;
+  else {
+    return Math.max(maxHeight(node.left), maxHeight(node.right)) + 1;
+  }
+}
+
+const values = [3, 9, 20, null, null, 15, 7];
+const root = buildBinaryTree(values);
+console.log(maxHeight(root));
+
+const values2 = [1, null, 2];
+const root2 = buildBinaryTree(values2);
+console.log(maxHeight(root2));
+
+/* Time Complexity: Time complexity of maxHeight function is O(N). For buildBinaryTree function, time complexity is O(N^2) as shift() method has O(N) and it is inside a while loop.
+     Space Complexity: O(N) as there is recursion.
+  */
---
+++ subtreeOfBST.js
@@ -0,0 +1,87 @@
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new TreeNode(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else this.insertNode(node.left, newNode);
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else this.insertNode(node.right, newNode);
+    }
+  }
+}
+
+function buildBST(tree) {
+  let BST = new BinarySearchTree();
+  for (let i = 0; i < tree.length; i++) {
+    BST.insert(tree[i]);
+  }
+  return BST.root;
+}
+
+function search(node, value) {
+  if (node === null) {
+    return null;
+  } else if (value < node.value) {
+    return search(node.left, value);
+  } else if (value > node.value) {
+    return search(node.right, value);
+  }
+  return node;
+}
+function bfsTraversal(root) {
+  let subTree = [];
+  if (root == null) {
+    return [];
+  }
+  let queue = [];
+  queue.push(root);
+  while (queue.length != 0) {
+    var tempNode = queue.shift();
+    subTree.push(tempNode.value);
+
+    if (tempNode.left != null) {
+      queue.push(tempNode.left);
+    }
+    if (tempNode.right != null) {
+      queue.push(tempNode.right);
+    }
+  }
+  return subTree;
+}
+let tree = [4, 2, 7, 1, 3];
+let val = 2;
+
+let root = buildBST(tree);
+let node = search(root, val);
+let subTree = bfsTraversal(node);
+console.log(subTree);
+
+let tree2 = [4, 2, 7, 1, 3];
+let val2 = 5;
+
+let root2 = buildBST(tree2);
+let node2 = search(root2, val2);
+let subTree2 = bfsTraversal(node2);
+console.log(subTree2);
